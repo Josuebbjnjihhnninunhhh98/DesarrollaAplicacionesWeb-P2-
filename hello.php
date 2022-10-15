9f7ed2c5087c42859b4ec14a30bef32e346ab1db
Josuebb <114453373+Josuebbjnjihhnninunhhh98@users.noreply.github.com>
2022-10-14 23:11:21 -0500

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hola Mundo PHP</title>
+  </head>
+  <body style="background-color:#33FFD7;">
+<center>
+  <h1>
 <?php 
 print("Hola a todos"); 
 echo "<br>";
@@ -5,3 +13,8 @@ print("Bienvenidos al mundo PHP");
 echo "<br>";
 printf("Soy Hackiman");
 ?>
+</h1>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwLtnP6-5QvGyHssfPs9HKqtBMyZqgepowuA&usqp=CAU">
+</center>
+  </body>
+</html>
